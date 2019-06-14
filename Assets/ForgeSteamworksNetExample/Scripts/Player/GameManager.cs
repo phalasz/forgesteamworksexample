a273d75b1a4c2802dddc33a526f82ed010121935
Peter Halasz <skinn3r@gmail.com>
2019-06-14 16:52:00 +0100

    Cleanup when client disconnects from host.

--- Assets/ForgeSteamworksNetExample/Scripts/Player/GameManager.cs
+++ Assets/ForgeSteamworksNetExample/Scripts/Player/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using BeardedManStudios.Forge.Networking;
 using BeardedManStudios.Forge.Networking.Unity;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -14,6 +16,33 @@ namespace ForgeSteamworksNETExample.Player
 		private void Start()
 		{
 			localPlayer = NetworkManager.Instance.InstantiatePlayer() as NetworkedPlayer;
+
+			if (!NetworkManager.Instance.IsServer)
+				return;
+
+			NetworkManager.Instance.Networker.playerDisconnected += (player, networker) =>
+			{
+				MainThreadManager.Run((() =>
+				{
+					List<NetworkObject> toDelete = new List<NetworkObject>();
+					foreach (var obj in networker.NetworkObjectList)
+					{
+						if (obj.Owner == player)
+						{
+							toDelete.Add(obj);
+						}
+					}
+
+					if (toDelete.Count > 0)
+					{
+						for (int i = toDelete.Count - 1; i >= 0; i--)
+						{
+							networker.NetworkObjectList.Remove(toDelete[i]);
+							toDelete[i].Destroy();
+						}
+					}
+				}));
+			};
 		}
 
 		private void Update()
@@ -24,6 +53,11 @@ namespace ForgeSteamworksNETExample.Player
 
 				SceneManager.LoadScene(0);
 			}
+		}
+
+		private void OnApplicationQuit()
+		{
+			NetworkManager.Instance.Disconnect();
 		}
 	}
 }
